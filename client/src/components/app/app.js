c95dff48eaf6fa7c0cef221fc1a49dfd73f852ac
alexey kostyukhin <aleksey4uk@gmail.com>
2020-08-03 13:26:33 +0300

    Добавил Проверку наличия "авторизации по токену"

--- client/src/components/app/app.js
+++ client/src/components/app/app.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import AuthPage from '../pages/auth-page';
 import {ErrorPage} from '../pages/error-page';
 import HomePage from '../pages/home-page';
 import AddNews from '../add-news'
 import DetailPage from '../pages/detail-page';
+import {getToken} from '../../utils/utils.js';
 
 import 'antd/dist/antd.css';
 import './app.css';
 
 function App() {
+  const token = getToken();
+  if(!token) return <Redirect to="/" /> 
+
   return (
     <div className="App">
       <Switch>
