708a540d840d0aa792c3b016408db86a4b0d9a8d
alexey kostyukhin <aleksey4uk@gmail.com>
2020-08-11 10:55:45 +0300

    tanks

--- models/user.js
+++ models/user.js
@@ -1,10 +1,13 @@
 const {Schema, model, Types} = require('mongoose');
 
 const schema = new Schema({
-    email: {type: String,required: true,unique: true}, 
+    email: {type: String,required: true,unique: true},
     password: {type: String, required: true},
-    news: [{ type: Types.ObjectId, ref: 'News' }]
+    news: [{ type: Types.ObjectId, ref: 'News' }],
+    img: {type: String},
+    name: {type: String, default: 'Не определено'},
+    surname: {type: String, default: 'asdasd'},
+    date: { type: Date, default: Date.now },
 })
-
 
 module.exports = model('User', schema)
